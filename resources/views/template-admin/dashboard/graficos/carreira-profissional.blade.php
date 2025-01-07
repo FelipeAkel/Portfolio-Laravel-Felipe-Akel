339d755e92bfd5063eaba93f584d741d908e31ef
Felipe Akel <95441539+FelipeAkel@users.noreply.github.com>
2025-01-07 19:48:38 -0300

    FEAT: Dashboard - Grafico Pie - Carreira Profissional

--- resources/views/template-admin/dashboard/graficos/carreira-profissional.blade.php
+++ resources/views/template-admin/dashboard/graficos/carreira-profissional.blade.php
@@ -3,7 +3,60 @@
         Carreira Profissional
     </div>
     <div class="card-body">
-        <h5 class="card-title ">Gráfico</h5>
+        <canvas id="graficoCarreiraProfissional"></canvas>
     </div>
     <div class="card-footer text-body-secondary"></div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    // Dados do gráfico
+    const data = {
+        labels: [
+        
+            @foreach ($totalCarreiraProfissional as $dadosCarreira )
+                '{{ $dadosCarreira->no_tipo_experiencia }}',
+            @endforeach
+        
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [
+                @foreach ($totalCarreiraProfissional as $dadosCarreira )
+                    {{ $dadosCarreira->total_quantidade }},
+                @endforeach
+            ],
+            hoverOffset: 4
+        }]
+    };
+
+    // Configuração do gráfico
+    const config = {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom', // Posição da legenda
+                },
+                tooltip: {
+                    callbacks: {
+                    label: function(context) {
+                        let label = context.label || '';
+                        let value = context.raw || 0;
+                        return `${label}: ${value}`;
+                    }
+                    }
+                }
+            }
+        }
+    };
+
+    // Renderizando o gráfico
+    const graficoCarreiraProfissional = new Chart(
+        document.getElementById('graficoCarreiraProfissional'),
+        config
+    );
+</script>
